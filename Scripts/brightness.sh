65e3d541c777c234d8d8fcfff1332cf15b5d4217
haller218 <eliseu42lucena@gmail.com>
2019-12-24 11:14:42 -0300

    new changes for brightness

--- Scripts/brightness.sh
+++ Scripts/brightness.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sudo echo 200 > /sys/class/backlight/intel_backlight/brightness
 
+ES=$1
+
+if [[ -e $ES ]]; then
+
+    sudo echo 150 > /sys/class/backlight/intel_backlight/brightness
+else
+    
+    sudo echo $ES > /sys/class/backlight/intel_backlight/brightness
+fi
