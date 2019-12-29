cd99f7be0dab3adc18d4f5083f3f8265f5dfb16c
haller218 <eliseu42lucena@gmail.com>
2019-12-29 13:40:55 -0300

    my emacs.d

---
+++ home/.emacs.d/elpa/code-archive-20190612.308/code-archive-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "code-archive" "20190612.308" "git supported code archive and reference for org-mode" '((emacs "24.3")) :commit "1ad9af6679d0294c3056eab9cad673f29c562721" :authors '(("Michael Schuldt" . "mbschuldt@gmail.com")) :maintainer '("Michael Schuldt" . "mbschuldt@gmail.com") :url "https://github.com/mschuldt/code-archive")
---
+++ home/.emacs.d/elpa/dr-racket-like-unicode-20161021.1211/dr-racket-like-unicode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dr-racket-like-unicode" "20161021.1211" "DrRacket-style unicode input" '((emacs "24.1")) :commit "4953f1c8a68472e157a0dcd0a7e35a4ec2577133" :keywords '("i18n" "tools") :authors '(("David Christiansen" . "david@davidchristiansen.dk")) :maintainer '("David Christiansen" . "david@davidchristiansen.dk"))
