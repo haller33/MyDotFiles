6c96da14949f9b4a3929103b66a3c546a8a5a94b
haller218 <eliseu42lucena@gmail.com>
2020-01-11 12:43:56 -0300

    comands for cryptokeyboard

--- Scripts/cryptoboard.sh
+++ Scripts/cryptoboard.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 #
 # /etc/init.d/configtokeyboardac
+#cd  /etc/init.d
 
+#wget https://pastebin.com/raw/vDPFGukB
+
+#mv vDPFGukB configtokeyboardac.sh
+
+#chmod +x configtokeyboardac.sh
+
+#/etc/init.d/configtokeyboardac.sh start
+
+#/etc/init.d/configtokeyboardac.sh stop
+
+#/etc/init.d/configtokeyboardac.sh restart
+
+#update-rc.d configtokeyboardac.sh defaults
+
+# exit
 setxkbmap -model abnt2 -layout br -variant abnt2
