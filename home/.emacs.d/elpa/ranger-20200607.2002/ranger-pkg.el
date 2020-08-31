b3718778d181ca342e731bf6898011af2d32ad0f
haller33 <eliseu42lucena@gmail.com>
2020-08-30 23:07:46 -0300

    add ranger and treemacs with configuration for emacs

---
+++ home/.emacs.d/elpa/ranger-20200607.2002/ranger-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ranger" "20200607.2002" "Make dired more like ranger" '((emacs "24.4")) :commit "caf75f0060e503af078c7e5bb50d9aaa508e6f3e" :keywords '("files" "convenience" "dired") :authors '(("Rich Alesi <https://github.com/ralesi>")) :maintainer '("Rich Alesi <https://github.com/ralesi>") :url "https://github.com/ralesi/ranger")
