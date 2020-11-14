7cc1e9bce36aa3b9484a371d9d8adfca822fd65c
haller33 <eliseu42lucena@gmail.com>
2020-11-14 16:57:27 -0300

    alterations for line and some others stuff

---
+++ home/.emacs.d/.emacs.d/elpa/rcirc-menu-1.1/rcirc-menu-autoloads.el
@@ -0,0 +1,27 @@
+;;; rcirc-menu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "rcirc-menu" "rcirc-menu.el" (0 0 0 0))
+;;; Generated autoloads from rcirc-menu.el
+
+(autoload 'rcirc-menu "rcirc-menu" "\
+Show a list of all your `rcirc' buffers.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rcirc-menu" '("rcirc-menu-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rcirc-menu-autoloads.el ends here
