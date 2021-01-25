daeacc485019a63e99b4747b49a26ab026efa8b7
haller33 <eliseu42lucena@gmail.com>
2021-01-24 22:12:14 -0300

    changes

--- Scripts/gotowork.sh
+++ Scripts/gotowork.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
+TIME=2
+
+FRASE="no process or procastination find"
+GOTOWORK="GOTOWORK"
+
+echo $FRASE
+
 while true; do
     
-    NS=$(flatpak ps | \grep --color telegram | tr '\t' ' ' | cut -d ' ' -f 1)    
+    NS=$(flatpak ps | grep telegram | tr '\t' ' ' | cut -d ' ' -f 1)    
 
     if [[ -n $NS ]]; then
 
 	flatpak kill $NS
+
 	echo "GOTOWORK"
-    else
-	echo "no process or procastination fold"
+
+#    else
+#	echo "no process or procastination fold"
+
     fi
-    sleep 2
+
+    sleep $TIME
 done
+
+
+
