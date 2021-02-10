40cad94907c7cdda392f97fac745efea652233c6
haller218 <eliseu42lucena@gmail.com>
2021-02-10 05:23:34 -0300

    remove recursive emacs.d bloat

--- home/.emacs.d/.emacs.d/elpa/webpaste-20191214.649/webpaste-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "webpaste" "20191214.649" "Paste to pastebin-like services" '((emacs "24.4") (request "0.2.0") (cl-lib "0.5")) :commit "8f9322fb1c6b8a80187f48ba2cb96cc107fad2b2" :keywords '("convenience" "comm" "paste") :authors '(("Elis \"etu\" Hirwing")) :maintainer '("Elis \"etu\" Hirwing") :url "https://github.com/etu/webpaste.el")
