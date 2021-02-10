34bb7b27fbd49308c2e65d6b498da56e53dd2e4c
haller218 <eliseu42lucena@gmail.com>
2021-02-10 05:23:34 -0300

    remove recursive emacs.d bloat

--- home/.emacs.d/.emacs.d/elpa/dired-subtree-20180922.1615/dired-subtree-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dired-subtree" "20180922.1615" "Insert subdirectories in a tree-like fashion" '((dash "2.5.0") (dired-hacks-utils "0.0.1")) :commit "f49a8bbf95f70671a74a24f7f4de453b2686be46" :keywords '("files") :authors '(("Matúš Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matúš Goljer" . "matus.goljer@gmail.com"))
